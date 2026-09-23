48c17265f8fd0df9b90b16a964fa27015b3ca341
rahmatullohsotiboldiev9-tech <rahmatullohsotiboldiev9@gmail.com>
2026-09-23 14:51:33 +0900

    feat:express integrations

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+console.log("Hello John bro!");
+
+interface SquareResult {
+    number: number;
+    square: number;
+}
+
+function getSquareNumbers(arr: number[]): SquareResult[] {
+    return arr.map((number) => ({
+        number: number,
+        square: number * number,
+    }));
+}
+
+console.log(getSquareNumbers([1, 2, 3]));
