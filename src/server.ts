0c4b5f2f7fa30c01bbfdfe92a1c05677889b0252
rahmatullohsotiboldiev9-tech <rahmatullohsotiboldiev9@gmail.com>
2026-09-23 12:49:48 +0900

    fix: integration of enviromental variables

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-// console.log("EXECUTED");
-
 // import moment from "moment";
 // const currentTime = moment().format("YYYY MM DD")
-
 // console.log(currentTime);
-
 // const person: string = "Martin";
 // const count: number = 100;
-
-
 // Architectural pattern: MVC: model view controller, DI:Dependency Injection , MVP:model view presenter
-
 // Design pattern: Middleware, Decotar
+
+import dotenv from 'dotenv'     //Oldingi const moment = require ('moment');
+dotenv.config();
+
+console.log("PORT:", process.env.PORT);
+
+console.log("MONGO_URL:", process.env.MONGO_URL);
